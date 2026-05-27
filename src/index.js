dde6cf7f4bc921af1a9901ed3ff987b2d80d8187
Queen Ashwini <queenashwini@Queens-MacBook-Pro-2.local>
2026-05-26 22:58:06 -0400

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BlueOriginProgram from './blue-origin-app';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BlueOriginProgram />
+  </React.StrictMode>
+);
